32fe1b228e234165ad00f968bc2049876b8f2fd9
ARounder <2892375641@qq.com>
2025-11-28 16:22:58 +0800

    feat: 添加 hello 和获取业务数据的 API 端点

--- home_application/urls.py
+++ home_application/urls.py
@@ -3,4 +3,5 @@ from home_application import views
 
 urlpatterns = [
     url(r'^hello/$', views.hello),
+    url(r'^get_biz_data/$', views.get_biz_data),
 ]
--- home_application/views.py
+++ home_application/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from blueking.component.shortcuts import get_client_by_request
+
 
 # Create your views here.
 def hello(request):
     return HttpResponse("Hello World!")
+
+def get_biz_data(request):
+    client = get_client_by_request(request)
+    kwargs = {'bk_biz_id': 1}
+    result = client.cc.list_biz_host(kwargs)
+    return JsonResponse(result)
